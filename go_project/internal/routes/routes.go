97c2387056a95a71113389595703d6052ee6b69f
Clinton-okumu <clintonomondiokumu@gmail.com>
2025-05-16 16:56:45 +0300

    feat: Building a go http server using chi.

--- go_project/internal/routes/routes.go
+++ go_project/internal/routes/routes.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/go-chi/chi/v5"
+	"go_project/internal/app"
+)
+
+func SetUpRoutes(app *app.Application) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", app.HealthChecker)
+	return r
+}
--- go_project/main.go
+++ go_project/main.go
@@ -1,6 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"go_project/internal/app"
+	"net/http"
+	"time"
+	"flag"
+	"go_project/internal/routes"
+)
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "Go backend server port")
+	flag.Parse()
 
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+	
+	r := routes.SetUpRoutes(app)
+
+	server :=
+		&http.Server{
+			Addr:         fmt.Sprintf(":%d", port),
+			Handler:      r,
+			IdleTimeout:  time.Minute,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 30 * time.Second,
+		}
+
+	app.Logger.Printf("Starting server on port %d", port)
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
 }
+
